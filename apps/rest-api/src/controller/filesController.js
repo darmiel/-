d34353fd2ce56592f4ac3905d0ee1008930f4b8e
darmiel <71837281+darmiel@users.noreply.github.com>
2020-11-26 14:07:23 +0100

    Implemented GET /files/:messageId and GET /files/:messageId/:fileId

--- apps/rest-api/src/controller/filesController.js
+++ apps/rest-api/src/controller/filesController.js
@@ -5,15 +5,65 @@ const Joi = require("joi");
 /*
  * Schemas
  */
+const fileUidSchema = Joi.string().min(1).required();
+const messageIdSchema = Joi.number().required();
 const fileSchema = Joi.object({
-  fileUid: Joi.string().min(1).required(),
-  messageId: Joi.number().required(),
+  fileUid: fileUidSchema,
+  messageId: messageIdSchema,
   cdn_path: Joi.string().required(),
   size: Joi.number().optional(),
   downloaded: Joi.number().required(),
 });
 
-// POST /messages
+module.exports.fileUidSchema = fileUidSchema;
+module.exports.messageIdSchema = messageIdSchema;
+
+// GET /files/:messageId/:fileId
+module.exports.getFile = async (messageId, fileUid) => {
+  const connection = await dbController.pool.getConnection();
+  try {
+    const rows = await connection.query(
+      "SELECT * FROM messages_files WHERE fileUid = ? AND messageId = ?;", 
+      [fileUid, parseInt(messageId)]
+    );
+    if (rows.length < 1) {
+      return {};
+    } else {
+      return rows[0];
+    }
+  } catch (exception) {
+    return {
+      error: true,
+      message: exception.code,
+    };
+  } finally {
+    if (connection) {
+      connection.end();
+    }
+  }
+};
+
+// GET /files/:messageId
+module.exports.getFiles = async (messageId) => {
+  const connection = await dbController.pool.getConnection();
+  try {
+    return await connection.query(
+      "SELECT * FROM messages_files WHERE messageId = ?;", 
+      [parseInt(messageId)]
+    );
+  } catch (exception) {
+    return {
+      error: true,
+      message: exception.code,
+    };
+  } finally {
+    if (connection) {
+      connection.end();
+    }
+  }
+};
+
+// POST /files
 module.exports.addFile = async (file) => {
   // validate user schema
   const { error, value } = fileSchema.validate(file);
--- apps/rest-api/src/routes/files.js
+++ apps/rest-api/src/routes/files.js
@@ -19,4 +19,41 @@ router.post("/", async (req, res) => {
   return res.status(controllerResult.error ? 400 : 200).json(controllerResult);
 });
 
+/**
+ * GET /files/:messageId
+ * Returns the files associated with the messageId
+ */
+router.get("/:messageId", async (req, res) => {
+  let r = valid.validate(res, Joi.number().min(0), req.params.messageId);
+  if (r == undefined) {
+    return;
+  }
+  const rows = await controller.getFiles(r);
+  if (rows == []) {
+    return res.status(404).json([]);
+  }
+  return res.status(200).json(rows);
+});
+
+/**
+ * GET /files/:messageId/:fileId
+ * Returns the files associated with the messageId
+ */
+router.get("/:messageId/:fileUid", async (req, res) => {
+  const messageId = valid.validate(res, controller.messageIdSchema, req.params.messageId);
+  if (messageId == undefined) {
+    return;
+  }
+  const fileUid = valid.validate(res, controller.fileUidSchema, req.params.fileUid);
+  if (fileUid == undefined) {
+    return;
+  }
+
+  const rows = await controller.getFile(messageId, fileUid);
+  if (rows == []) {
+    return res.status(404).json([]);
+  }
+  return res.status(200).json(rows);
+});
+
 module.exports = router;
